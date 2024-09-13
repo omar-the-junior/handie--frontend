7725cb6c2c8b2cf051a89d7a2ca5e482d572ce82
abanob rafik <bobarafik0123@gmail.com>
2024-09-13 19:54:50 +0300

    Made all changes that you told me

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import StarRating from './components/stars';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <StarRating rating={0} />
   </StrictMode>,
 );
