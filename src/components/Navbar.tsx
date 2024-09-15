2632543092f5d3ccd9ab44747cbb3081209cbaa9
Abdulla Ahmed <ahmedmoustafaabdelwahed@gmail.com>
2024-09-15 06:34:24 +0300

    3-create navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,38 +4,29 @@ import { useState } from 'react';
 import { Icon } from '@iconify/react/dist/iconify.js';
 
 function Navbar() {
-  const [isloggedin, setisloggedin] = useState(false);
-  const [isdropdown, setisdropdown] = useState(false);
-  const [isseller, setisseller] = useState(false); //get from seller page data
-  const [ismenuopen, setismenuopen] = useState(false);
-  const toggoledropdown = () => {
-    setisdropdown(!isdropdown);
+  const [IsLoggedIn, SetIsLoggedIn] = useState(false);
+  const [IsDropDown, SetIsDropDown] = useState(false);
+  const [IsSeller, SetIsSeller] = useState(false); //get from seller page data
+  const [IsMenuOpen, SetIsMenuOpen] = useState(false);
+  const Toggoledropdown = () => {
+    SetIsDropDown(!IsDropDown);
   };
   const toggleMobileMenu = () => {
-    setismenuopen(!ismenuopen);
-    if (ismenuopen) {
-      setisdropdown(false); 
+    SetIsMenuOpen(!IsMenuOpen);
+    if (IsMenuOpen) {
+      SetIsDropDown(false);
     }
   };
 
   return (
     <nav
-      className="flex items-center justify-between"
-      style={{
-        width: '100%',
-        padding: '20px 80px',
-        borderBottom: '2px solid #B88E2F',
-      }}
+      className="flex items-center justify-flex items-center justify-between w-full py-5 px-20 border-b-2 border-[#B88E2F]"
     >
 
       <div className="block lg:hidden">
         <button onClick={toggleMobileMenu}>
-          <Icon icon="ci:menu-alt-02" width="40" height="40" className="text-black" style={{
-            position: 'absolute',
-            top: '30px',
-            right: '20px',
-
-          }} />
+          <Icon icon="ci:menu-alt-02" width="40" height="40" className="text-black absolute top-[30px] right-[20px]"
+          />
         </button>
       </div>
 
@@ -43,10 +34,9 @@ function Navbar() {
 
 
 
-      {ismenuopen && (
+      {IsMenuOpen && (
         <div
-          className="fixed top-0 right-0 h-full bg-white shadow-lg z-50"
-          style={{ width: '250px', padding: '20px', borderLeft: '2px solid #B88E2F' }}
+          className="fixed top-0 right-0 h-full bg-white shadow-lg z-50 w-[250px] p-5 border-l-2 border-[#B88E2F]"
         >
           <button onClick={toggleMobileMenu} className="absolute top-4 right-4">
             <Icon icon="ci:close-big" width="30" height="30" className="text-black" />
@@ -70,38 +60,29 @@ function Navbar() {
 
           {/* Arrange sell and login buttons in a column */}
           <div className="flex flex-col items-start space-y-4 mt-8">
-            {isseller && isloggedin ? (
+            {IsSeller && IsLoggedIn ? (
               <img src="/sell.svg" alt="Sell" />
             ) : (
-              <div onClick={() => setisseller(!isseller)}>
+              <div onClick={() => SetIsSeller(!IsSeller)}>
                 <Button size="small" variant="solid" color="primary" className="w-24">
                   Sell
                 </Button>
               </div>
             )}
 
-            {isloggedin ? (
+            {IsLoggedIn ? (
               <>
                 <Icon
                   icon="iconamoon:profile-circle"
                   width="64"
                   height="64"
                   className="h-10 w-10 rounded-full text-primary"
-                  onClick={toggoledropdown}
+                  onClick={Toggoledropdown}
                 />
 
-                {isdropdown && (
+                {IsDropDown && (
                   <div
-                    className="flex flex-col mt-2 bg-white shadow-lg rounded-lg dropdown"
-                    style={{
-                      padding: '16px',
-                      border: '1px solid #E8E8E8',
-                      zIndex: '1000',
-                      backgroundColor: '#F6F0E4',
-                      position: 'absolute',
-                      right: '10px',
-                      top: '100px',
-                    }}
+                    className="flex flex-col mt-2 bg-[#F6F0E4] shadow-lg rounded-lg border border-[#E8E8E8] z-[1000] absolute right-[10px] top-[100px] p-4 dropdown"
                   >
                     <ul className="flex flex-col space-y-4">
                       <li className="flex items-center space-x-2">
@@ -121,7 +102,7 @@ function Navbar() {
                 )}
               </>
             ) : (
-              <div onClick={() => setisloggedin(!isloggedin)}>
+              <div onClick={() => SetIsLoggedIn(!IsLoggedIn)}>
                 <Button size="small" variant="outline" color="primary" className="w-24">
                   Login
                 </Button>
@@ -140,7 +121,7 @@ function Navbar() {
         <img src="/images/logo-horizontal.svg" className="w-40" alt="logo" />
       </Link>
 
-      {/* <div className={`flex-grow ${ismenuopen ? 'hidden lg:flex' : 'flex'}`}> */}
+      {/* <div className={`flex-grow ${IsMenuOpen ? 'hidden lg:flex' : 'flex'}`}> */}
       <ul className="flex items-center space-x-10">
         <li className="text-sm font-semibold leading-normal text-black">
           <Link to="/">Home</Link>
@@ -157,17 +138,17 @@ function Navbar() {
       </ul>
 
       <div className="flex items-center space-x-4">
-        {isseller && isloggedin ? (
+        {IsSeller && IsLoggedIn ? (
           <img src="/sell.svg" alt="Sell" />
         ) : (
-          <div onClick={() => setisseller(!isseller)}>
+          <div onClick={() => SetIsSeller(!IsSeller)}>
             <Button size="small" variant="solid" color="primary" className="w-24">
               Sell
             </Button>
           </div>
         )}
 
-        {isloggedin ? (
+        {IsLoggedIn ? (
           <>
             {/* Wrap the icon and dropdown must in fragment */}
             <Icon
@@ -175,22 +156,12 @@ function Navbar() {
               width="64"
               height="64"
               className="h-10 w-10 rounded-full text-primary"
-              onClick={toggoledropdown}
+              onClick={Toggoledropdown}
             />
 
-            {isdropdown && (
+            {IsDropDown && (
               <div
-                className="flex mt-2  bg-white shadow-lg rounded-lg dropdown"
-                style={{
-                  padding: '16px',
-                  border: '1px solid #E8E8E8',
-                  zIndex: '1000',
-                  backgroundColor: '#F6F0E4',
-                  position: 'absolute',
-                  right: '10px',
-                  top: '100px',
-
-                }}
+                className="flex mt-2 bg-[#F6F0E4] shadow-lg rounded-lg border border-[#E8E8E8] z-[1000] absolute right-[10px] top-[100px] p-4 dropdown"
               >
                 <ul className="flex flex-col space-y-4">
                   <li className="flex items-center space-x-2">
@@ -210,7 +181,7 @@ function Navbar() {
             )}
           </>
         ) : (
-          <div onClick={() => setisloggedin(!isloggedin)}>
+          <div onClick={() => SetIsLoggedIn(!IsLoggedIn)}>
             <Button size="small" variant="outline" color="primary" className="w-24">
               Login
             </Button>
