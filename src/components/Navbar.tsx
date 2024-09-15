b0857b555d587caa5d59bde3cf6f823ab1de62ca
Abdulla Ahmed <ahmedmoustafaabdelwahed@gmail.com>
2024-09-15 03:11:56 +0300

    done the task

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,16 @@ import { Link } from 'react-router-dom';
 import Button from './Button';
 import { useState } from 'react';
 import { Icon } from '@iconify/react/dist/iconify.js';
+
 function Navbar() {
-  const [isloggedin, setisloggedin] = useState(true);
+  const [isloggedin, setisloggedin] = useState(false);
+  const [isdropdown, setisdropdown] = useState(false);
+  const [isseller, setisseller] = useState(false); //get from seller page data
+
+  const toggoledropdown = () => {
+    setisdropdown(!isdropdown);
+  };
+
   return (
     <nav
       className="flex items-center justify-between"
@@ -30,30 +38,66 @@ function Navbar() {
           <Link to="/contact">Contact</Link>
         </li>
       </ul>
-      <div className="flex items-center space-x-4">
-        <Button size="small" variant="solid" color="primary" className="w-24">
-          Sell
-        </Button>
 
-        {isloggedin ? (
+      <div className="flex items-center space-x-4">
+        {isseller && isloggedin ? (
+          <img src="/sell.svg" alt="Sell" />
+        ) : (
+          <div onClick={() => setisseller(!isseller)}>
+            <Button size="small" variant="solid" color="primary" className="w-24">
+              Sell
+            </Button>
+          </div>
+        )}
+
+
+      {isloggedin ? (
+        <>
+          {/* Wrap the icon and dropdown must in fragment */}
           <Icon
             icon="iconamoon:profile-circle"
             width="64"
             height="64"
             className="h-10 w-10 rounded-full text-primary"
+            onClick={toggoledropdown}
           />
-        ) : (
-          <Button
-            size="small"
-            variant="outline"
-            color="primary"
-            className="w-24"
-          >
+
+          {isdropdown && (
+            <div
+              className="flex mt-2  bg-white shadow-lg rounded-lg dropdown"
+              style={{
+                padding: '16px',
+                border: '1px solid #E8E8E8',
+                zIndex: '1000',
+                backgroundColor: '#F6F0E4',
+              }}
+            >
+              <ul className="flex flex-col space-y-4">
+                <li className="flex items-center space-x-2">
+                  <img src="/shopping_cart.svg" alt="Wishlist Icon" className="w-6 h-6" />
+                  <span className="font-semibold text-black">Wishlist</span>
+                </li>
+                <li className="flex items-center space-x-2">
+                  <img src="/wishlist.svg" alt="Shopping Cart Icon" className="w-6 h-6" />
+                  <span className="font-semibold text-black">Shopping cart</span>
+                </li>
+                <li className="flex items-center space-x-2">
+                  <img src="/profile.svg" alt="Profile Icon" className="w-6 h-6" />
+                  <span className="font-semibold text-black">Profile</span>
+                </li>
+              </ul>
+            </div>
+          )}
+        </>
+      ) : (
+        <div onClick={() => setisloggedin(!isloggedin)}>
+          <Button size="small" variant="outline" color="primary" className="w-24">
             Login
           </Button>
-        )}
-      </div>
-    </nav>
+        </div>
+      )}
+    </div>
+    </nav >
   );
 }
 
