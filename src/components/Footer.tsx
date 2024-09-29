b2aa905f962593857587c70d8d0ca57db87d8da3
abanob rafik <bobarafik0123@gmail.com>
2024-09-29 19:02:52 +0300

    edit the src of img, the text size, the logo & use link from router-dom

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
+import { Link } from 'react-router-dom';
+
 const Footer = () => {
-  const logoVertical = '../public/images/logo-vertical.svg';
-  const style = 'font-medium text-neutral text-base ';
+  const logoVertical = '/images/logo-vertical.svg';
+  const style = 'font-medium text-neutral text-base-r ';
 
   return (
     <footer className="w-full bg-secondary py-20">
@@ -9,8 +11,8 @@ const Footer = () => {
         <div className="md:flex-row flex flex-col items-start justify-between gap-12 self-stretch border-b-2 border-neutral px-0 py-5">
           {/* logo & description */}
           <div className="md:max-w-sm w-full flex-shrink-0">
-            <img src={logoVertical} alt="logo" sizes="36" />
-            <p className="text-base text-neutral">
+            <img src={logoVertical} alt="logo" className='size-36' />
+            <p className="text-base-r text-neutral">
               Handie is your go-to platform for discovering unique, handmade
               creations crafted with passion. We connect talented makers with
               buyers who value creativity and quality.
@@ -21,35 +23,35 @@ const Footer = () => {
           <div className="flex w-full justify-between lg:contents">
             <div className="md:gap-12 flex flex-col gap-7">
               {/* links */}
-              <h3 className="text-base font-semibold text-charcoal">Links</h3>
+              <h3 className="text-base-r font-semibold text-charcoal">Links</h3>
               <ul className="flex flex-col gap-7">
                 <li className={style}>
-                  <a href="">Home</a>
+                  <Link to="/">Home</Link>
                 </li>
                 <li className={style}>
-                  <a href="">Shop</a>
+                  <Link to="/shop">Shop</Link>
                 </li>
                 <li className={style}>
-                  <a href="">About</a>
+                  <Link to="/about">About</Link>
                 </li>
                 <li className={style}>
-                  <a href="">Contact</a>
+                  <Link to="/contact">Contact</Link>
                 </li>
               </ul>
             </div>
 
             {/* help */}
             <div className="md:gap-12 flex flex-col gap-7">
-              <h3 className="text-base font-semibold text-charcoal">Help</h3>
+              <h3 className="text-base-r font-semibold text-charcoal">Help</h3>
               <ul className="flex flex-col gap-7">
                 <li className={style}>
-                  <a href="">Payment Options</a>
+                  <a href="#">Payment Options</a>
                 </li>
                 <li className={style}>
-                  <a href="">Returns</a>
+                  <a href="#">Returns</a>
                 </li>
                 <li className={style}>
-                  <a href="">Privacy Policies</a>
+                  <a href="#">Privacy Policies</a>
                 </li>
               </ul>
             </div>
@@ -58,7 +60,7 @@ const Footer = () => {
 
         {/* footer bottom */}
         <div>
-          <p className="text-base font-normal text-charcoal">
+          <p className="text-base-r font-normal text-charcoal">
             &copy;2024 Handie. All rights reserved
           </p>
         </div>
