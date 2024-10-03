61ad218529861ff21ef13644ed533d342ba7314c
omar-the-junior <omarmasoudonline@gmail.com>
2024-10-03 22:51:31 +0300

    Refactor signup page layout and styles

--- src/routes/Signup.tsx
+++ src/routes/Signup.tsx
@@ -1,20 +1,19 @@
-import { Link } from 'react-router-dom';
 import { Input, InputGroup } from '../components/Input';
 
 function Signup() {
   return (
-    <div className="min-h-screen bg-white py-8">
-      <div className="container grid grid-cols-2 place-items-center gap-8">
+    <div className="py-8">
+      <div className="container grid place-items-center gap-8 lg:grid-cols-2">
         <img
-          className="hidden max-h-[713px] w-full rounded-md md:block"
-          src="/images/signup.svg"
+          className="hidden max-h-[713px] w-full rounded-md lg:block"
+          src="/images/signup.jpg"
           alt="Handmade Marketplace"
           height="713"
           width="535"
         />
 
-        <div className="w-full max-w-lg ">
-          <h1 className="mb-4  text-3xl font-bold text-primary ">
+        <div className="w-full md:max-w-lg">
+          <h1 className="h3 mb-4 font-bold text-primary">
             Welcome to Your Handmade Marketplace
           </h1>
           <p className="mb-8 text-charcoal">
@@ -22,7 +21,7 @@ function Signup() {
           </p>
 
           <form className="grid gap-10">
-            <InputGroup className="flex-1">
+            <InputGroup>
               <Input
                 id="name"
                 variant="simple"
@@ -31,7 +30,7 @@ function Signup() {
               />
             </InputGroup>
 
-            <InputGroup className="flex-1">
+            <InputGroup>
               <Input
                 id="email"
                 variant="simple"
@@ -40,7 +39,7 @@ function Signup() {
               />
             </InputGroup>
 
-            <InputGroup className="flex-1">
+            <InputGroup>
               <Input
                 id="password"
                 variant="simple"
@@ -49,7 +48,7 @@ function Signup() {
               />
             </InputGroup>
 
-            <InputGroup className="flex-1">
+            <InputGroup>
               <Input
                 id="password"
                 variant="simple"
@@ -58,15 +57,15 @@ function Signup() {
               />
             </InputGroup>
 
-            <button className="btn mt-6 w-full">Sign up</button>
-            <div className=" flex items-center">
+            <button className="btn mt-12 w-full">Sign up</button>
+            <div className="flex items-center">
               <span className="text-sm">Already have an account?</span>
-              <Link
-                to="/login"
+              <a
+                href="/login"
                 className="ml-1 text-sm font-bold text-primary hover:underline"
               >
                 Login
-              </Link>
+              </a>
             </div>
           </form>
         </div>
