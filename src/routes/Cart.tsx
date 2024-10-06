a2e722263f63ce20435b0111fe0bb5272098f39e
Abdulla Ahmed <ahmedmoustafaabdelwahed@gmail.com>
2024-10-06 18:40:22 +0300

    check the cart page

--- src/routes/Cart.tsx
+++ src/routes/Cart.tsx
@@ -3,38 +3,40 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const cartItems: any[] = [
-  // {
-  //   id: 1,
-  //   imageSrc:
-  //     'https://images.unsplash.com/photo-1567016432779-094069958ea5?q=80&w=2680&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  //   price: 15000,
-  //   name: 'Sofa',
-  //   quantity: 1,
-  // },
-  // {
-  //   id: 1,
-  //   imageSrc:
-  //     'https://images.unsplash.com/photo-1567016432779-094069958ea5?q=80&w=2680&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  //   price: 15000,
-  //   name: 'Sofa',
-  //   quantity: 1,
-  // },
-  // {
-  //   id: 1,
-  //   imageSrc:
-  //     'https://images.unsplash.com/photo-1567016432779-094069958ea5?q=80&w=2680&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  //   price: 15000,
-  //   name: 'Sofa',
-  //   quantity: 1,
-  // },
-  // {
-  //   id: 1,
-  //   imageSrc:
-  //     'https://images.unsplash.com/photo-1567016432779-094069958ea5?q=80&w=2680&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  //   price: 15000,
-  //   name: 'Sofa',
-  //   quantity: 1,
-  // },
+  {
+    id: 1,
+    imageSrc:
+      'https://images.unsplash.com/photo-1567016432779-094069958ea5?q=80&w=2680&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    price: 15000,
+    name: 'Sofa',
+    quantity: 1,
+  },
+  {
+    id: 1,
+    imageSrc:
+      'https://images.unsplash.com/photo-1567016432779-094069958ea5?q=80&w=2680&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    price: 15000,
+    name: 'Sofa',
+    quantity: 1,
+  },
+  {
+    id: 1,
+    imageSrc:
+      'https://images.unsplash.com/photo-1567016432779-094069958ea5?q=80&w=2680&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    price: 15000,
+    name: 'Sofa',
+    quantity: 1,
+  },
+  {
+
+    
+    id: 1,
+    imageSrc:
+      'https://images.unsplash.com/photo-1567016432779-094069958ea5?q=80&w=2680&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    price: 15000,
+    name: 'Sofa',
+    quantity: 1,
+  },
 ];
 
 function Cart() {
