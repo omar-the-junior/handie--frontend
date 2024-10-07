8c1aa19279d0170f1a9d3563559dd716cf43b378
abanob rafik <bobarafik0123@gmail.com>
2024-10-07 18:21:24 +0300

    make become seller

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,12 +1,8 @@
-import BecomeSeller from "../components/BecomeSeller";
-
 function Home() {
   return (
     <div>
-      {/* <h1>My Website</h1>
-      <p>Welcome to the electronics section of our store!</p> */}
-
-      <BecomeSeller />
+      <h1>My Website</h1>
+      <p>Welcome to the electronics section of our store!</p>
     </div>
   );
 }
