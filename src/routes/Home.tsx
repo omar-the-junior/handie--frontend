d4b310659ab63fc9b1738184971aca3f3ca1ed87
omar-the-junior <omarmasoudonline@gmail.com>
2024-10-09 00:19:59 +0300

    chore: return the `Home` route to it's original state

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,11 @@
-function Home() {
-  return (
-    <div>
-      <h1>My Website</h1>
-      <p>Welcome to the electronics section of our store!</p>
-    </div>
-  );
+function Home()
+{
+    return (
+    
+        <div>
+          <h1>My Website</h1>
+          <p>Welcome to the electronics section of our store!</p>
+        </div>
+    )
 }
 export default Home;
